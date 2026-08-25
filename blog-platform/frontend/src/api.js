e1c372f825ab43d10cb0bf1478bdaa64c045daa2
kavipriya-k12 <kavipriyakpka@gmail.com>
2026-08-25 12:36:17 +0530

    Connect frontend to Render backend

--- blog-platform/frontend/src/api.js
+++ blog-platform/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blog-platform-h3e1.onrender.com/"
+  baseURL: "https://blog-platform-h3e1.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
